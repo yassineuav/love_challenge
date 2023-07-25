acd8deb16c53b32f43577e3dd19e2b595117b1ca
Yassine Khiadouch <yassine.uav@gmail.com>
2023-07-25 03:42:02 -0700

    login sign in pages adding api appwrite

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,11 +1,36 @@
 import 'package:app/core/core.dart';
 import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as model;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+
+
+final authAPIProvider = Provider((ref){});
+
 
 abstract class IAuthAPI {
-  FutureEither<Account> signUp({
+  FutureEither<model.Account> signUp({
     required String email,
     required String password,
   });
 }
 
+class AuthAPI implements IAuthAPI {
+  final Account _account;
+  AuthAPI({required Account account}) : _account = account;
 
+  @override
+  FutureEither<model.Account> signUp(
+      {required String email, required String password}) async {
+    try {
+      final account = await _account.create(
+          userId: "unique()", email: email, password: password);
+      return right(account);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+          Failure(e.message ?? 'Some unexpected error occurred', stackTrace));
+    } catch (e, stackTrace) {
+      return left(Failure(e.toString(), stackTrace));
+    }
+  }
+}
